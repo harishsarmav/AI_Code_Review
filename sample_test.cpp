fad9521536f5b70a0fc373cfd0bf1db3a3ef0339
Harish Sarma <101693424+harishsarmav@users.noreply.github.com>
2024-12-14 22:23:40 +0530

    Update sample_test.cpp

--- sample_test.cpp
+++ sample_test.cpp
@@ -30,9 +30,6 @@ int main() {
     
     Rectangle *rect = new Rectangle(width, height);
     cout << "The area is: " << rect->area() << endl;
-
-
-
     
     // Intentionally missing delete statement
     saveAreaToFile(rect);
